72826f0395558122ea3d49a661acb975502c85ac
Yoana Penkova <56016797+yoadimpen@users.noreply.github.com>
2022-04-08 18:29:56 +0200

    Initial upload

---
+++ src/test/java/tfm/alzi/AlziApplicationTests.java
@@ -0,0 +1,13 @@
+package tfm.alzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
