96025a4a7b61b37931638ce81adcc7ac77f31026
Yoana <yoadimpen@alum.us.es>
2022-05-04 16:11:55 +0200

    S1 - P007: calendar added now
    calendar view in personal area added, no recordatorio entities yet

--- src/main/java/tfm/alzi/controllers/GenericController.java
+++ src/main/java/tfm/alzi/controllers/GenericController.java
@@ -30,29 +30,41 @@ public class GenericController {
     @RequestMapping("/area-personal")
     public String goToAreaPersonal(final Model model, final HttpServletRequest request) {
         if (request.getUserPrincipal() != null) {
+			return "areaPersonal";
+		} else {
+            return "login";
+        } 
+    }
+
+    @RequestMapping("/entrenamiento")
+    public String goToEntrenamiento(final Model model, final HttpServletRequest request) {
+        if (request.getUserPrincipal() != null) {
+			return "entrenamiento";
+		} else {
+            return "login";
+        } 
+    }
+
+    @RequestMapping("/perfil")
+    public String goToPerfil(final Model model, final HttpServletRequest request) {
+        if (request.getUserPrincipal() != null) {
             Principal principal = request.getUserPrincipal();
             String dni = principal.getName();
             Usuario usuario = this.usuarioService.getUsuarioByDNI(dni);
             model.addAttribute("usuario", usuario);
-			return "areaPersonal";
+			return "perfil";
 		} else {
             return "login";
         }
     }
 
-    @RequestMapping("/entrenamiento")
-    public String goToEntrenamiento() {
-        return "entrenamiento";
-    }
-
-    @RequestMapping("/perfil")
-    public String goToPerfil() {
-        return "perfil";
-    }
-
     @RequestMapping("/ajustes")
-    public String goToAjustes() {
-        return "ajustes";
+    public String goToAjustes(final Model model, final HttpServletRequest request) {
+        if (request.getUserPrincipal() != null) {
+			return "ajustes";
+		} else {
+            return "login";
+        } 
     }
 
 }
